7a8f650e2e69e26d73fda87851c081fe1e23064b
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2021-09-21 15:28:45 -0500

    Completed getTheTitles

--- javascriptExercises/11_getTheTitles/getTheTitles.js
+++ javascriptExercises/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const answer = books.map(book => book.title);
 
+    return answer;
 };
 
 module.exports = getTheTitles;
