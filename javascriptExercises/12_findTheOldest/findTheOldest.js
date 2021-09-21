892858a89302a2e3f861081b83762625eed70696
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2021-09-21 16:10:21 -0500

    Finished findTheOldest Challenge

--- javascriptExercises/12_findTheOldest/findTheOldest.js
+++ javascriptExercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
-};
+    for(let person in people) {
+      if ('yearOfDeath' in people[person] === false) {
+        people[person].yearOfDeath = new Date().getFullYear();
+      }
+    }
+  
+  
+  
+      const oldest = people.sort(function(a, b) {
+          const lastPerson = a.yearOfDeath - a.yearOfBirth;
+          const nextPerson = b.yearOfDeath - b.yearOfBirth;
+          return lastPerson > nextPerson ? -1 : 1;
+        });
+      
+      return oldest[0];
+  
+  };
 
 module.exports = findTheOldest;
