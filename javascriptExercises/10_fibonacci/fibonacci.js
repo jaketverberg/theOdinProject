c0d268ef505624305bcc622fecaf2c64043f0e41
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2021-09-21 15:21:47 -0500

    Finished Fibonaci

--- javascriptExercises/10_fibonacci/fibonacci.js
+++ javascriptExercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    let fibStart = [1, 1];
+  
+    if (n < 0) {
+      return "OOPS";
+    }
+  
+    for (let i = 0; i <= n-3; i++) {
+      let lastFib = fibStart[fibStart.length-1];
+      let secondLastFib = fibStart[fibStart.length-2];
+      let toPushFib = lastFib + secondLastFib;
+  
+      fibStart.push(toPushFib);
+    }
+  
+  return fibStart[fibStart.length-1];
+  
+  };
 
 module.exports = fibonacci;
