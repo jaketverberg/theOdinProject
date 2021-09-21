fd6f5df42569ac5b17b5ec1205b4bfb6f2ed7ba2
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2021-09-21 14:56:59 -0500

    Finished Palindrome challenge

--- javascriptExercises/09_palindromes/palindromes.js
+++ javascriptExercises/09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (pali) {
+    let reduced = pali.toLowerCase()
+                    .replace(/[^\w\s]/gi, "")
+                    .replace(/ /g, "")
 
+    let reversed = reduced.split("")
+                            .reverse()
+                            .join("");
+
+    return (reduced === reversed) ? true : false;
 };
 
 module.exports = palindromes;
