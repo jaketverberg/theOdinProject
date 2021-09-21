141080e8f49aa05f0a22e9568d576ba62633a744
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2021-09-21 14:25:46 -0500

    Working through calculator

--- javascriptExercises/08_calculator/calculator.js
+++ javascriptExercises/08_calculator/calculator.js
@@ -1,25 +1,42 @@
 const add = function() {
-	
+	return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1]
 };
 
 const sum = function() {
-	
-};
+	const argu = [...arguments];
+
+  let answer = argu.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue
+    });
+  
+  return answer;
+  };
 
 const multiply = function() {
-
+	const argu = [...arguments];
+  
+  let answer = argu.reduce((previousValue, currentValue) => {
+    return previousValue * currentValue
+    });
+  
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+
 };
 
 module.exports = {
